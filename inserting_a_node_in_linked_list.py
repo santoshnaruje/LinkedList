ba94be0d2cde4e8076eb5bcac2687ced7f4d85d3
santoshnaruje <santoshnaruje@gmail.com>
2026-01-22 08:28:28 +0530

    Updated inserting a node at given index
    1.Insert a node at given position
    2.Insert a node at given value

--- inserting_a_node_in_linked_list.py
+++ inserting_a_node_in_linked_list.py
@@ -1,10 +1,54 @@
-from main import printLinkedList
+from main import printLinkedList, convertArrayToLinkedList
 
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+def insertNodeAtGivenPosition(head, position, val):
+    node = Node(val)
+
+    if position == 0:
+        node.next = head
+        return node
+
+    currentNode = head
+    count = 0
+    previousNode = None
+
+    while currentNode:
+        if count == position:
+            previousNode.next = node
+            node.next = currentNode
+            return head
+        previousNode = currentNode
+        currentNode = currentNode.next
+        count += 1
+
+    previousNode.next = node
+    return head
+
+
+def insertNodeBeforeValue(head, val, new_val):
+    node = Node(new_val)
+    if head.data == val:
+        node.next = head
+        return node
+
+    previousNode = None
+    currentNode = head
+    while currentNode:
+        if currentNode.data == val:
+            previousNode.next = node
+            node.next = currentNode
+            return head
+
+        previousNode = currentNode
+        currentNode = currentNode.next
+
+    return head
 
 
 def insertNodeInLinkedList(head, data):
@@ -18,7 +62,7 @@ def insertNodeInLinkedList(head, data):
 
 
 if __name__ == '__main__':
-    head = Node(10)
-    head.next = Node(20)
-    head = insertNodeInLinkedList(head, 30)
+    array = [1, 2, 3, 4, 5]
+    head = convertArrayToLinkedList(array)
+    head = insertNodeAtGivenPosition(head, 5, 6)
     printLinkedList(head)
